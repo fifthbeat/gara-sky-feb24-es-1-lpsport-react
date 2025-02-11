9b692cbd5b1bc66824e21564267530d59bd2e2ef
a.devita <a.devita@dstech.it>
2025-02-11 15:40:41 +0100

    feat: added children in button

--- src/components/Atoms/Button/index.tsx
+++ src/components/Atoms/Button/index.tsx
@@ -1,24 +1,26 @@
+import {ReactNode} from "react";
+
 interface ButtonProps {
   as: "button" | "link";
+  children: string | ReactNode;
   variant: string;
-  disabled: boolean;
-  text: string;
+  disabled?: boolean;
   onClick?: () => void;
   href?: string;
 }
 
-const Button = ({as = 'button', variant, disabled, text, onClick, href}: ButtonProps) => {
+const Button = ({children, as = 'button', variant, disabled, onClick, href}: ButtonProps) => {
 
   if (as === "button") {
     return (
       <button className={variant} disabled={disabled} onClick={onClick}>
-        {text}
+        {children}
       </button>
     )
   } else if (as === "link") {
     return (
       <a href={href} className={variant}>
-        {text}
+        {children}
       </a>
     )
   }
