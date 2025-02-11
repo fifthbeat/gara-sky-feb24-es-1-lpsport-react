f7985579ea734ad58c3584e0f71981c975a3241b
m-tamburro <m.tamburro@dst.it>
2025-02-11 15:48:30 +0100

    feat: added style to price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,48 @@
 import Image from "next/image";
 import Button from "@/components/Atoms/Button";
+import Price from "@/components/Atoms/Price";
 
 export default function Home() {
   return (
       <>
-        <div style={{margin: "5rem", backgroundColor: "lightgray", padding: "1rem", display: "flex", gap: "1rem"}}>
-          <Button
-              as="button"
-              variant="btn btn-primary"
-              disabled={false}
-              text="Click me"
-              onClick={() => {}}
-          />
-          <Button
-              as="button"
-              variant="btn btn-secondary"
-              disabled={false}
-              text="Click me"
-              onClick={() => {}}
+        <div style={{margin: "5rem", backgroundColor: "white", padding: "1rem", display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <h1 className="gradient-primary">Price</h1>
+          <Price
+              price="24,90"
+              priceCurrency={"€"}
+              pricePeriod="/mese"
+              priceNote="IVA esclusa"
+              variant="primary"
           />
         </div>
-        <div style={{margin: "5rem", backgroundColor: "lightgray", padding: "1rem", display: "flex", gap: "1rem"}}>
-          <Button
-              as="button"
-              variant="btn btn-primary btn-disabled"
-              disabled={false}
-              text="Click me"
-              onClick={() => {}}
-          />
-          <Button
-              as="button"
-              variant="btn btn-secondary btn-disabled"
-              disabled={false}
-              text="Click me"
-              onClick={() => {}}
-          />
+
+        <div style={{margin: "5rem", backgroundColor: "white", padding: "1rem", display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem"}}>
+          <h1>Buttons</h1>
+          <div style={{display: "flex", gap: "1rem"}}>
+            <Button
+                as="button"
+                variant="btn btn-primary"
+                disabled={false}
+                text="Click me"
+            />
+            <Button
+                as="button"
+                variant="btn btn-secondary"
+                disabled={false}
+                text="Click me"
+            />
+            <Button
+                as="button"
+                variant="btn btn-primary btn-disabled"
+                disabled={false}
+                text="Click me"
+            />
+            <Button
+                as="button"
+                variant="btn btn-secondary btn-disabled"
+                disabled={false}
+                text="Click me"
+            /></div>
         </div>
       </>
   );
--- src/components/Atoms/Price/index.tsx
+++ src/components/Atoms/Price/index.tsx
@@ -6,17 +6,23 @@ interface PriceProps {
   variant?: string;
 }
 
-const Price = ({ price, priceCurrency, pricePeriod, priceNote, variant }: PriceProps) => {
+const Price = ({price, priceCurrency, pricePeriod, priceNote, variant}: PriceProps) => {
   return (
-    <div className="price-offer">
-      <span className={"price" + "_" + variant}>
-        <span className="price-value">{price.split(",")[0]}</span>
-        <span className="price-decimal">,{price.split(",")[1]}</span>
-        <span className="price-currency">{priceCurrency}</span>
-      </span>
-      <span className="price-period">{pricePeriod}</span>
-      <span className="price-note">{priceNote}</span>
-    </div>
+      <>
+        <div className="price-offer">
+          <span className={"gradient-primary price price" + "_" + variant}>
+            <span className="price-value">{price.split(",")[0]}</span>
+            <div className="price-right">
+              <div className="currency-right">
+              <span className="price-decimal">,{price.split(",")[1]}</span>
+              <span className="price-currency">{priceCurrency}</span>
+            </div>
+              <span className="price-period">{pricePeriod}</span>
+            </div>
+          </span>
+          <span className="price-note">{priceNote}</span>
+        </div>
+      </>
   );
 };
 
